c215c565d195de308b6c4db33e790f657817036a
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-07 12:47:46 +0530

    video count on category ui

--- client/src/components/categories/CategoryCard.jsx
+++ client/src/components/categories/CategoryCard.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { useVideo } from "../../store/VideoStore";
+import { MdOutlinePlaylistPlay } from "react-icons/md";
 
 function CategoryCard({ item }) {
   const { getISTdate } = useVideo();
   return (
-    <div className="w-[300px]">
-      <img
-        src={item.image}
-        className="w-[300px] h-[169.3px] object-cover rounded-[10px]"
-      />
+    <div className="w-[300px] cursor-pointer">
+      <div className="relative">
+        <img
+          src={item.image}
+          className="hover:opacity-[.7] transition-all ease-linear duration-200 w-[300px] h-[169.3px] object-cover rounded-[10px]"
+        />
+        <div className="bg-[black] px-[5px] py-[2px] rounded-[5px] flex gap-[3px] items-center absolute bottom-[5px] right-[10px]">
+          <MdOutlinePlaylistPlay />{" "}
+          <p className="text-[10px] relative top-[-.6px]">
+            {item.videoCount} videos
+          </p>
+        </div>
+      </div>
       <div className="px-[5px]">
         <div className="flex justify-between items-center mt-[3px]">
           <p className="text-[14px] font-[500]">{item.name}</p>
