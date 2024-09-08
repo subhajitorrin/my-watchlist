2d2bd33f43a4e36f068d7a3513a924758ebd73dc
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-08 16:26:14 +0530

    working on search functionality...

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useVideo } from "../../store/VideoStore";
 
 function Search() {
+  const { searchLoading, searchedList } = useVideo((state) => ({
+    searchLoading: state.searchLoading,
+    searchedList: state.searchedList
+  }));
   return (
     <div className="absolute h-full w-full bg-[rgb(10,10,10)] z-[12] rounded-[7px]"></div>
   );
