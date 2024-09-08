24cbb868f12cdeb365ab4485c466a8e5d69b7bb7
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-08 20:36:55 +0530

    working on search functionality...

---
+++ client/src/hook/useDebounce.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+function useDebounce(value, delay = 500) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(id);
+    };
+  }, [value, delay]);
+  return debouncedValue;
+}
+
+export default useDebounce;
