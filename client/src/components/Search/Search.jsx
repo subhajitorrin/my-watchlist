7d645e0dff34afd2cf2bd204ea147dfca465bdf5
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-08 16:39:11 +0530

    working on search functionality...

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -3,6 +3,7 @@ import { useVideo } from "../../store/VideoStore";
 import { SyncLoader } from "react-spinners";
 import { CgSearch } from "react-icons/cg";
 import SearchCard from "./SearchCard";
+import VideoCard from "../Home/VideoCard";
 
 function Search() {
   const { searchLoading, searchedList, library } = useVideo((state) => ({
@@ -11,7 +12,7 @@ function Search() {
     library: state.library
   }));
   return (
-    <div className="absolute  w-full bg-[rgb(10,10,10)] z-[12] rounded-[7px]">
+    <div className="p-[10px] absolute  w-full bg-[rgb(10,10,10)] z-[12] rounded-[7px]">
       {searchLoading ? (
         <div className=" h-[200px] flex flex-col gap-[20px] items-center justify-center">
           <SyncLoader color="#ffffff" size={5} />
@@ -20,7 +21,7 @@ function Search() {
           </p>
         </div>
       ) : (
-        <div className="">
+        <div className="max-h-[80vh] flex flex-col gap-[10px]">
           {library.map((item, index) => {
             return <SearchCard key={index} item={item} />;
           })}
--- client/src/components/Search/SearchCard.jsx
+++ client/src/components/Search/SearchCard.jsx
@@ -1,9 +1,108 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useVideo } from "../../store/VideoStore";
+import { toast } from "react-toastify";
+import { BeatLoader } from "react-spinners";
 
-function SearchCard() {
+function SearchCard({ item }) {
+  const [deleteLoad, setDeleteLoad] = useState(false);
+  const [addQueueLoad, setAddQueueLoad] = useState(false);
+  const { formatTime, getISTdate, getISTtime, deleteVideo, addVideoToQueue } =
+    useVideo();
+
+  async function handleAddVideoToQueue() {
+    setAddQueueLoad(true);
+    try {
+      await addVideoToQueue(item._id);
+      toast.success("Added to queue");
+    } catch (error) {
+      console.log(error);
+      toast.warn(error.response?.data?.message || error.message);
+    } finally {
+      setAddQueueLoad(false);
+    }
+  }
+
+  async function handleDeleteVideo() {
+    setDeleteLoad(true);
+    try {
+      await deleteVideo(item._id);
+      toast.success("Video deleted");
+    } catch (error) {
+      toast.warn(error.response?.data?.message || error.message);
+    } finally {
+      setDeleteLoad(false);
+    }
+  }
+
   return (
-    <div>SearchCard</div>
-  )
+    <div className=" flex gap-[10px] rounded-[7px] min-h-[150px] w-full bg-[#111827] p-[10px]">
+      <div className="h-full w-[35%]">
+        <img
+          className="h-full w-full object-cover rounded-[7px]"
+          src={item.thumbnail}
+        />
+      </div>
+      <div className="w-[75%] flex flex-col justify-between">
+        <div className="">
+          <h2 className="text-[16px] font-[500] text-[#ffffffba]">
+            {item.title.length > 115 ? (
+              <>
+                {item.title.slice(0, 115)}
+                <span>...</span>
+              </>
+            ) : (
+              item.title
+            )}
+          </h2>
+          <div className="flex gap-[10px]">
+            <p className="text-[13px] ">{formatTime(item.duration)}</p>
+            <p className="text-[13px] text-[#ffffff96]">
+              Added {getISTdate(item.createdAt)},{" "}
+              <span>{getISTtime(item.createdAt)}</span>
+              {/* Added 05-09-2024, <span></span> */}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex gap-[7px]">
+          {item.tags.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="text-[11px] bg-[#374f86] inline-block rounded-[5px] px-[7px] py-[2px]"
+              >
+                {item}
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="flex gap-[7px] items-center ">
+          <button
+            style={{ addQueueLoad: deleteLoad ? "none" : "auto" }}
+            onClick={handleAddVideoToQueue}
+            className="bg-[#7e22ce] font-[500] hover:bg-[#6018a0] transition-all ease-linear duration-200 py-[3px] px-[10px] rounded-[4px] text-[12px]"
+          >
+            {addQueueLoad ? (
+              <BeatLoader color="#ffffff" size={5} />
+            ) : (
+              "Add to queue"
+            )}
+          </button>
+          <button className="bg-[#4ca8af] font-[500] hover:bg-[#3a8186] transition-all ease-linear duration-200 py-[3px] px-[10px] rounded-[4px] text-[12px]">
+            Reminder
+          </button>
+          <button
+            onClick={handleDeleteVideo}
+            style={{ pointerEvents: deleteLoad ? "none" : "auto" }}
+            className="bg-[#f44336] font-[500] hover:bg-[#b42828] transition-all ease-linear duration-200 py-[3px] w-[60px] rounded-[4px] text-[12px]"
+          >
+            {deleteLoad ? <BeatLoader color="#ffffff" size={5} /> : "Delete"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SearchCard
+export default SearchCard;
