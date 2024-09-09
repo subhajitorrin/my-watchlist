c0e2aaada78cbc7f3ea9f73fa7f313c19bd5c0df
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-09 21:26:24 +0530

    working on responsive...

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,13 +2,19 @@ import React, { useEffect } from "react";
 import HomeLeft from "./HomeLeft";
 import HomeRight from "./HomeRight";
 import Navbar from "../Navbar";
-import { useVideo } from "../../store/VideoStore";
+import { useWidth } from "../../store/useWIdth";
 
 function Home() {
+  const { isMobile } = useWidth((state) => ({
+    isMobile: state.isMobile
+  }));
   return (
     <>
       <Navbar />
-      <div className="h-[calc(100%-60px)] justify-center flex gap-[20px] px-[3%] py-[20px]">
+      <div
+        style={{ flexDirection: isMobile ? "" : "" }}
+        className="h-[calc(100%-60px)] justify-center flex gap-[20px] px-[3%] py-[20px]"
+      >
         <HomeLeft />
         <HomeRight />
       </div>
