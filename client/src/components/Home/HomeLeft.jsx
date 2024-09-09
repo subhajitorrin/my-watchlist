b934685862940bfee9bc26de5b0c44477da21c2b
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-09 21:31:58 +0530

    working on responsive...

--- client/src/components/Home/HomeLeft.jsx
+++ client/src/components/Home/HomeLeft.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
 import VideoCard from "./VideoCard";
 import { useVideo } from "../../store/VideoStore";
+import { useWidth } from "../../store/useWIdth";
 
 function HomeLeft() {
+  const { isMobile } = useWidth((state) => ({
+    isMobile: state.isMobile
+  }));
   const {
     library,
     setDropDownIndex,
@@ -20,6 +24,44 @@ function HomeLeft() {
   useEffect(() => {
     getLibrary();
   }, [homeDropDownValue]);
+
+  if (isMobile) {
+    return <div className="h-full w-full gap-[10px] flex flex-col">
+      <div className="flex justify-between items-center">
+        <h2 className="text-[20px] font-[500]">My Library</h2>
+        <select
+          onChange={(e) => {
+            setDropDownIndex(e.target.value);
+          }}
+          value={homeDropDownValue}
+          className="text-white bg-[#111827] text-[14px] font-[500] outline-none w-[200px] py-[3px] rounded-[5px] px-[5px]"
+        >
+          {homeDropDownList.map((item, index) => {
+            return (
+              <option
+                className="font-[500] cursor-pointer"
+                value={item.value}
+                key={index}
+              >
+                {item.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div className="flex flex-col gap-[10px] h-[100%] overflow-y-auto scrollNone">
+        {library.length === 0 ? (
+          <div className="flex justify-center items-center rounded-[7px] h-[150px] w-full bg-[#111827]">
+            <p className="text-[#ffffff8a]">No video in library</p>
+          </div>
+        ) : (
+          library.map((item, index) => {
+            return <VideoCard key={index} item={item} />;
+          })
+        )}
+      </div>
+    </div>;
+  }
 
   return (
     <div className="h-full w-[50%] gap-[10px] flex flex-col">
