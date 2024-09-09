8b488a3e24b59f30ec0cdf6731273dbf92ab4869
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-09 21:42:09 +0530

    working on responsive...

--- client/src/components/Home/HomeRight.jsx
+++ client/src/components/Home/HomeRight.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import AddVideo from "./AddVideo";
 import NowPlaying from "./NowPlaying";
+import { useWidth } from "../../store/useWIdth";
 
 function HomeRight() {
+  const { isMobile } = useWidth((state) => ({
+    isMobile: state.isMobile
+  }));
   return (
     <div className="h-full w-[30%] flex flex-col gap-[20px]">
       <AddVideo />
-      <NowPlaying/>
+      <NowPlaying />
     </div>
   );
 }
