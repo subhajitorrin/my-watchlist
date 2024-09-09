480dc5ab56e2333a19ee7814cc09689affc5c120
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-09 21:42:30 +0530

    working on responsive...

--- client/src/components/Home/HomeRight.jsx
+++ client/src/components/Home/HomeRight.jsx
@@ -7,6 +7,13 @@ function HomeRight() {
   const { isMobile } = useWidth((state) => ({
     isMobile: state.isMobile
   }));
+  if (isMobile) {
+    return (
+      <div className="h-full w-[30%] flex flex-col gap-[20px]">
+        <AddVideo />
+      </div>
+    );
+  }
   return (
     <div className="h-full w-[30%] flex flex-col gap-[20px]">
       <AddVideo />
